90a2316c1cb85ffb97e6ffb735dce64c376f1f07
Raul Morales <irmoralesb@hotmail.com>
2021-04-06 17:35:24 -0500

    Adding Unit Tests

--- rest_api/controllers/booksController.js
+++ rest_api/controllers/booksController.js
@@ -1,10 +1,14 @@
 function booksController(Book) {
     function post(req, res) {
-        console.log(req.body);
         const book = new Book(req.body);
+        if(!req.body.title){
+            res.status(400);
+            return res.send('Title is required');
+        }
+        
         book.save();
-        console.log(book);
-        return res.status(201).json(book);
+        res.status(201)
+        return res.json(book);
     }
     function get(req, res) {
         const query = {};
